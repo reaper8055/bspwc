bfacf95338f32c861290fc47163813d64255655d
Simon Zeni <simonzeni@gmail.com>
2018-01-13 09:58:24 -0500

    Add argument parsing

--- bway/main.c
+++ bway/main.c
@@ -1,8 +1,56 @@
+#include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BWAY_VERSION 0.1 // TODO: Move that elsewhere
 
 int main(int argc, char *argv[])
 {
-    printf("Hello World!\n");
+    static int verbose = 0;
+
+    char* config_path = NULL;
+
+    const char* usage = 
+        "Usage: bway [option]\n"
+        "\n"
+        " -h, --help\t\tShow this message\n"
+        " -c, --config\t\tSpecify a configuration file\n"
+        " -v, --version\t\tShow the version number and exits\n"
+        " -V, --verbose\t\tEnable verbose output\n"
+        "\n";
+
+    static struct option long_options[] = {
+        {"help", no_argument, NULL, 'h'},
+        {"config", required_argument, NULL, 'c'},
+        {"version", no_argument, NULL, 'v'},
+        {"verbose", no_argument, NULL, 'V'},
+        {0, 0, 0, 0}
+    };
+
+    int opt;
+    while ((opt = getopt_long(argc, argv, "hcvV:", long_options, NULL)) != -1)
+    {
+        switch (opt)
+        {
+            case 'h':
+                fprintf(stdout, "%s", usage);
+			    exit(EXIT_SUCCESS);
+                break;
+            case 'c':
+                config_path = malloc(strlen(optarg) + 1);
+                strcpy(config_path, optarg);
+                break;
+            case 'v':
+                printf("%f\n", BWAY_VERSION);
+                exit(EXIT_SUCCESS);
+                break;
+            case 'V':
+                verbose = 1;
+                break;
+        }
+    }
 
     return 0;
 }
