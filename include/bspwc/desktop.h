a2050c3bd1ef78d740a4b9383102b63d0f62672a
Simon Zeni <simonzeni@gmail.com>
2018-01-23 20:02:26 -0400

    Add desktop type members and padding struct

--- include/bspwc/desktop.h
+++ include/bspwc/desktop.h
@@ -3,9 +3,21 @@
 
 #include <wlr/types/wlr_output.h>
 
+struct padding
+{
+	int top;
+	int right;
+	int bottom;
+	int left;
+};
+
 struct desktop
 {
+    char name[32];
     struct wlr_output* output;
+    struct desktop* prev;
+    struct desktop* next;
+    struct padding padding;
 };
 
 struct desktop* desktop_create(struct desktop* d);
