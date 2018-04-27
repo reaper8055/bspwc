365afd6dab04a2feb5f23d60b49659bcb49460ea
Simon Zeni <simonzeni@gmail.com>
2018-04-27 16:07:16 -0300

    Add cursor callbacks

--- bspwc/cursor.c
+++ bspwc/cursor.c
@@ -1,5 +1,24 @@
 #include "bspwc/cursor.h"
 
+void handle_cursor_motion(struct wl_listener* listener, void* data)
+{
+	struct cursor* cursor = wl_container_of(listener, cursor, motion);
+	struct wlr_event_pointer_motion* event = data;
+
+	wlr_log(L_DEBUG, "Cursor %d, %d", event->delta_x, event->delta_y);
+
+	wlr_cursor_move(cursor->cursor, event->device, event->delta_x, event->delta_y);
+}
+
+void handle_cursor_motion_absolute(struct wl_listener* listener, void* data)
+{}
+
+void handle_cursor_button(struct wl_listener* listener, void* data)
+{}
+
+void handle_cursor_axis(struct wl_listener* listener, void* data)
+{}
+
 struct cursor* create_cursor(struct input* input)
 {
 	struct cursor* cursor = calloc(1, sizeof(struct cursor));
@@ -50,6 +69,19 @@ struct cursor* create_cursor(struct input* input)
 		);
 
 	wlr_cursor_warp(cursor->cursor, NULL, layout_box->width / 2 , layout_box->height / 2);
+
+	// Setup callbacks
+	wl_signal_add(&cursor->cursor->events.motion, &cursor->motion);
+	cursor->motion.notify = handle_cursor_motion;
+
+	wl_signal_add(&cursor->cursor->events.motion_absolute, &cursor->motion_absolute);
+	cursor->motion_absolute.notify = handle_cursor_motion_absolute;
+
+	wl_signal_add(&cursor->cursor->events.button, &cursor->button);
+	cursor->button.notify = handle_cursor_button;
+
+	wl_signal_add(&cursor->cursor->events.axis, &cursor->axis);
+	cursor->axis.notify = handle_cursor_axis;
 
 	return cursor;
 }
--- include/bspwc/cursor.h
+++ include/bspwc/cursor.h
@@ -1,6 +1,7 @@
 #ifndef CURSOR_H
 #define CURSOR_H
 
+#include <wayland-server.h>
 #include <wlr/types/wlr_cursor.h>
 #include <wlr/types/wlr_xcursor_manager.h>
 
@@ -12,7 +13,17 @@ struct cursor
 
 	struct wlr_cursor* cursor;
 	struct wlr_xcursor* xcursor;
+
+	struct wl_listener motion;
+	struct wl_listener motion_absolute;
+	struct wl_listener button;
+	struct wl_listener axis;
 };
+
+void handle_cursor_motion(struct wl_listener* listener, void* data);
+void handle_cursor_motion_absolute(struct wl_listener* listener, void* data);
+void handle_cursor_button(struct wl_listener* listener, void* data);
+void handle_cursor_axis(struct wl_listener* listener, void* data);
 
 struct cursor* create_cursor(struct input* input);
 void destroy_cursor(struct cursor* cursor);
