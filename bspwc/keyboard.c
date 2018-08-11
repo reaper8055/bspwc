935e58621457ab84bddbef86df5c1ffa351ddccb
Keno Goertz <keno@goertz-berlin.com>
2018-08-11 18:15:43 +0200

    Print key name

--- bspwc/keyboard.c
+++ bspwc/keyboard.c
@@ -10,8 +10,20 @@ void handle_keyboard_key(struct wl_listener* listener, void* data)
 	struct keyboard* keyboard = wl_container_of(listener, keyboard, key);
 	struct wlr_event_keyboard_key* event = data;
 
+	struct xkb_state *state = xkb_state_new(keyboard->device->keyboard->keymap);
+	if (state == NULL)
+	{
+		wlr_log(WLR_ERROR, "Failed to create xkb state");
+	}
+
+	// Have to add 8 to convert libinput keycode to libxkbcommon
 	xkb_keycode_t keycode = event->keycode + 8;
-	wlr_log(WLR_DEBUG, "Key pressed");
+	xkb_keysym_t keysym = xkb_state_key_get_one_sym(state, keycode);
+
+	char keysym_name[64];
+	xkb_keysym_get_name(keysym, keysym_name, sizeof(keysym_name));
+
+	wlr_log(WLR_DEBUG, "%s", keysym_name);
 }
 
 void handle_keyboard_modifiers(struct keyboard* keyboard)
